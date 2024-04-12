7b97ac4add6111a89d17b56d9dba506ea4cdf3ee
Person098123 <143832410+Person098123@users.noreply.github.com>
2024-04-12 10:43:27 -0700

    Fixed timer problem, But will need to fix it not disappearing when hp = 0 and making the lose text also disappear

--- function.js
+++ function.js
@@ -6,16 +6,18 @@ function hideFunction(){
 //this sets the timer for the battle function changing whaterver text 
 var sec = 30;
 function timer(){
-    setInterval(function(){
+   var timers = setInterval(function(){
         document.getElementById('safeTimerDisplay').innerHTML='Time: 00:'+sec;
         sec--;
         
-        if (sec < 0) { 
+        if (sec <= 0) { 
              document.getElementById("yan").style.display="block";
              document.getElementById('safeTimerDisplay').innerHTML='Time: 00:'+sec;
              document.getElementById("first-battle").style.display="none";
-            
-       
+             document.getElementById('safeTimerDisplay').style.display= "none";
+             document.getElementById('retry').style.display= "block";
+            sec = 30
+            clearInterval(timers)
         }
         
        
@@ -32,24 +34,23 @@ function timer(){
         value--;
         document.getElementById('number').value = value
         if(value==0){
-            document.getElementById("nay").style.display="block"
-            
+            document.getElementById("nay").style.display="block";
+            document.getElementsById("yan").style.display="none";
+            document.getElementById('safeTimerDisplay').style.display= "none";
         }
     }
 
     function reveal(){
         document.getElementById("number").style.display="block"
         document.getElementById("ches-choose").style.display="none";
-         
+        document.getElementById("safeTimerDisplay").style.display="block";
+        document.getElementById("first-battle").style.display="block";
     }
 
     function retry(){
-        document.getElementById("ches-choose").style.display="block"
-        document.getElementById('safeTimerDisplay').style.display= "block";
-        document.getElementById('safeTimerDisplay').innerHTML='Time: 00:'+sec;
-        document.getElementById("first-battle").style.display="block";
-     
-        
+        document.getElementById("ches-choose").style.display="block" 
+        document.getElementById('retry').style.display= "none";
+        document.getElementById("yan").style.display="none";
     };
         
     
